c17cbcfb76f2038a51f98db5b79dd966b763954a
jackking <jackkingcc@gmail.com>
2018-06-14 07:11:04 +0800

    Initial project

---
+++ src/main/entity/Student.java
@@ -0,0 +1,39 @@
+package entity;
+
+public class Student {
+    private int id;
+    private String name;
+    private int sex;
+
+    public Student(){
+    }
+    public Student(int id, String name, int sex) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
