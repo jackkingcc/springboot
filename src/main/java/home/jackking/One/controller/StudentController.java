2394a304db9b85a5dc6c4ac8ebbc3228cee31163
e604847 <jjin4@statestreet.com>
2018-07-09 14:05:15 +0800

    Add form validation for Student

--- src/main/java/home/jackking/One/controller/StudentController.java
+++ src/main/java/home/jackking/One/controller/StudentController.java
@@ -3,18 +3,29 @@ package home.jackking.One.controller;
 import home.jackking.One.repository.Student;
 import home.jackking.One.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-public class StudentController {
+@Controller
+public class StudentController implements WebMvcConfigurer {
 
     @Autowired
     private StudentService studentService;
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/student-results").setViewName("student-results");
+    }
+
     @RequestMapping("/listStudent")
+    @ResponseBody
     public List<Student> list(){
         List<Student> res=new ArrayList<>();
 
@@ -24,15 +35,28 @@ public class StudentController {
     }
 
     @RequestMapping("/allStudent")
+    @ResponseBody
     public Iterable<Student> all(){
         return  studentService.all();
     }
 
     @GetMapping("/addStudent")
+    @ResponseBody
     public String saveStudent(@RequestParam String  name,@RequestParam int sex){
         Student student = new Student(name,sex);
         studentService.save(student);
         return "saved";
+    }
 
+    @GetMapping("/login-student")
+    public String loginStudentForm(Student student){
+        return "student";
+    }
+    @PostMapping("/login-student")
+    public String checkStudentInfo(@Valid Student student,BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "student";
+        }
+        return "redirect:/student-results";
     }
 }
--- src/main/java/home/jackking/One/repository/Student.java
+++ src/main/java/home/jackking/One/repository/Student.java
@@ -3,13 +3,22 @@ package home.jackking.One.repository;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Student {
     @Id
     @GeneratedValue
     private int id;
+    @NotNull
+    @Size(min=2,max=20)
     private String name;
+    @NotNull
+    @Max(1)
+    @Min(0)
     private int sex;
     private Long count;
 
